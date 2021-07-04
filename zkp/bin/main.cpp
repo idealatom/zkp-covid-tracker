016fc39fbe6cd63f8f4d7b4f0753df97f1b7b9bd
idealatom <idealatom@gmail.com>
2021-07-04 20:54:09 +0700

    update formula for circuit, add arguments for cli tool

--- zkp/bin/main.cpp
+++ zkp/bin/main.cpp
@@ -48,33 +48,71 @@ void pack_verifier_data(
     poutf.close();
 }
 
-blueprint<field_type> get_blueprint(std::size_t x, std::size_t y) {
+blueprint<field_type> get_blueprint(float ax, float ay, float r, float x, float y) {
     blueprint<field_type> bp;
     LocationCircuit circuit(bp);
     circuit.generate_r1cs_constraints(bp);
-    circuit.generate_r1cs_witness(bp, x, y);
-
-    cout << "X = " << x << endl;
-    cout << "Y = " << y << endl;
-    cout << "Blueprint is satisfied: " << bp.is_satisfied() << endl;
-
+    circuit.generate_r1cs_witness(bp, ax, ay, r, x, y);
     return bp;
 }
 
+bool validate_inputs(float ax, float ay, float r, float x, float y) {
+    // The latitude must be a number between -90 and 90
+    // The longitude must be a number between -180 and 180
+    if (abs(ax) > 90) {
+        cout << "Invalid area center latitude" << endl;
+        return false;
+    }
+    if (abs(x) > 90) {
+        cout << "Invalid user latitude" << endl;
+        return false;
+    }
+    if (abs(ay) > 180) {
+        cout << "Invalid area center longitude" << endl;
+        return false;
+    }
+    if (abs(ay) > 180) {
+        cout << "Invalid user longitude" << endl;
+        return false;
+    }
+    if (r > 100 || r <= 0) {
+        cout << "Radius should be in a range 0...100" << endl;
+        return false;
+    }
+
+    return true;
+}
+
 int main(int argc, char *argv[]) {
-    size_t x, y;
+    float x, y, r, ax, ay;
 
     boost::program_options::options_description options("CLI Proof Generator");
     options.add_options()
-    ("x,x", boost::program_options::value<size_t>(&x)->default_value(0))
-    ("y,y", boost::program_options::value<size_t>(&y)->default_value(0));
+    ("x,x", boost::program_options::value<float>(&x)->default_value(0))
+    ("y,y", boost::program_options::value<float>(&y)->default_value(0))
+    ("r,r", boost::program_options::value<float>(&r)->default_value(10))
+    ("a,ax", boost::program_options::value<float>(&ax)->default_value(0))
+    ("b,ay", boost::program_options::value<float>(&ay)->default_value(0));
 
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(options).run(), vm);
     boost::program_options::notify(vm);
 
+    cout << "Area: (" << ax << ", " << ay <<  ") radius=" << r << endl;
+    cout << "User coords: (" << x << ", " << y <<  ")" << endl;
+
+    cout << "Validating input..." << endl;
+    if (!validate_inputs(ax, ay, r, x, y)) {
+        return -1;
+    }
+
     cout << "Getting blueprint..." << endl;
-    blueprint<field_type> bp = get_blueprint(x, y);
+    blueprint<field_type> bp = get_blueprint(ax, ay, r, x, y);
+
+    cout << "Blueprint is satisfied: " << bp.is_satisfied() << endl;
+    if (!bp.is_satisfied()) {
+        return -1;
+    }
 
     cout << "Generating constraint system..." << endl;
     const r1cs_constraint_system<field_type> constraint_system = bp.get_constraint_system();
