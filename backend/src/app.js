dd42609b1b0b3cb8ce8c2174116b5576a7332dff
idealatom <idealatom@gmail.com>
2021-07-09 13:26:33 +0300

    backend api changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,41 +1,37 @@
-import express from 'express';
-import helmet from 'helmet';
-import fs from 'fs/promises';
 import cp from 'child_process';
 import util from 'util';
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 import { coerce, number, object, string, validate } from 'superstruct';
-import { customAlphabet } from 'nanoid';
 
-const nanoid = customAlphabet('0123456789abcdef', 8);
 const exec = util.promisify(cp.exec);
 const app = express();
+app.use(cors());
 app.use(helmet());
 
 const CLI_COMMAND = 'npx cli';
-const PWD = '/tmp';
 
-const NumberFromString = coerce(number(), string(), (s) => parseInt(s, 10));
+// const NumberFromString = coerce(number(), string(), (s) => parseInt(s, 10));
+const FloatFromString = coerce(number(), string(), (s) => parseFloat(s));
 
 const Query = object({
-  latMin: NumberFromString,
-  latMax: NumberFromString,
-  lngMin: NumberFromString,
-  lngMax: NumberFromString,
-  posLat: NumberFromString,
-  posLng: NumberFromString,
+  minLat: FloatFromString,
+  maxLat: FloatFromString,
+  minLng: FloatFromString,
+  maxLng: FloatFromString,
+  posLat: FloatFromString,
+  posLng: FloatFromString,
 });
 
 const argsFromObject = (object) =>
   Object.keys(object).reduce((acc, x) => `${acc} -${x} ${object[x]}`, '');
 
 const generateProof = async (coords) => {
-  const name = `proof_${nanoid()}`;
-  await exec(`${CLI_COMMAND} ${argsFromObject(coords)} > ${PWD}/${name}`);
-  return name;
+  const { stdout } = await exec(`${CLI_COMMAND} ${argsFromObject(coords)}`);
+  // `${CLI_COMMAND} ${argsFromObject(coords)} | xxd -p | tr -d '\n'`
+  return stdout;
 };
-
-const getFile = (name) => fs.readFile(`${PWD}/${name}`);
-const deleteFile = (name) => fs.unlink(`${PWD}/${name}`);
 
 app.get('/proof/generate', async (req, res) => {
   const [error, query] = validate(req.query, Query, { coerce: true });
@@ -43,12 +39,8 @@ app.get('/proof/generate', async (req, res) => {
   if (error) {
     res.json({ success: false, errors: error.failures() });
   } else {
-    const filename = await generateProof(query);
-    const proof = await getFile(filename);
-    await deleteFile(filename);
-    res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
-    res.type('application/octet-stream');
-    res.send(proof);
+    const proof = await generateProof(query);
+    res.json({ hex: proof });
   }
 });
 
