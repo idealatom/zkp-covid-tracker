57416fc4dddaa514c9b3960c589ad082c5511f7a
idealatom <idealatom@gmail.com>
2021-07-09 16:42:38 +0700

    rollback to checking min/max longitude and latitude

--- zkp/bin/circuit.h
+++ zkp/bin/circuit.h
@@ -14,109 +14,103 @@ float roundoff(float value, uint prec)
     return roundf(value * pow_10) / pow_10;
 }
 
-int convert_input(float value, uint prec) {
-    int pow_10 = pow(10, prec);
-    return int(roundoff(value, prec) * pow_10);
+int convert_input(float value) {
+    // to keep all coordinates positive shift them
+    uint INPUT_SHIFT = 1000;
+    uint PRECISION = 6;
+    int pow_10 = pow(10, PRECISION);
+    return int(roundoff(value + INPUT_SHIFT, PRECISION) * pow_10);
 }
 
 template<typename field_type>
 class LocationCircuit {
 
     public:
-        blueprint_variable<field_type> areaX, areaY;
-        blueprint_variable<field_type> radius;
+        blueprint_variable<field_type> minLat, maxLat;
+        blueprint_variable<field_type> minLng, maxLng;
+        blueprint_variable<field_type> posLat, posLng;
         blueprint_variable<field_type> out;
-        blueprint_variable<field_type> userX, userY;
 
-        blueprint_variable<field_type> deltaX, deltaY, deltaSum;
-        blueprint_variable<field_type> deltaX_square, deltaY_square, radius_square;
-        blueprint_variable<field_type> less, lessOrEq;
+        blueprint_variable<field_type> minLatLess, minLatLessOrEq, maxLatLess, maxLatLessOrEq;
+        blueprint_variable<field_type> minLngLess, minLngLessOrEq, maxLngLess, maxLngLessOrEq;
 
-        std::shared_ptr<comparison<field_type>> cmp;
+        std::shared_ptr<comparison<field_type>> minLatCmp, maxLatCmp, minLngCmp, maxLngCmp;
 
     LocationCircuit(blueprint<field_type> &bp) {
-        areaX.allocate(bp);
-        areaY.allocate(bp);
-        radius.allocate(bp);
+        minLat.allocate(bp);
+        maxLat.allocate(bp);
+        minLng.allocate(bp);
+        maxLng.allocate(bp);
 
         out.allocate(bp);
 
-        userX.allocate(bp);
-        userY.allocate(bp);
+        posLat.allocate(bp);
+        posLng.allocate(bp);
 
-        deltaX.allocate(bp);
-        deltaY.allocate(bp);
-        deltaSum.allocate(bp);
-        deltaX_square.allocate(bp);
-        deltaY_square.allocate(bp);
-        radius_square.allocate(bp);
-        less.allocate(bp);
-        lessOrEq.allocate(bp);
+        minLatLess.allocate(bp);
+        maxLatLess.allocate(bp);
+        minLngLess.allocate(bp);
+        maxLngLess.allocate(bp);
+        minLatLessOrEq.allocate(bp);
+        maxLatLessOrEq.allocate(bp);
+        minLngLessOrEq.allocate(bp);
+        maxLngLessOrEq.allocate(bp);
 
-        bp.set_input_sizes(4);
+        bp.set_input_sizes(1);
     }
 
     void generate_r1cs_constraints(blueprint<field_type> &bp) {
-        std::size_t n = 150;
+        std::size_t comparison_n = 150;
 
-        // (userX - areaX)^2 + (userY - areaY)^2 <= radius^2.
+        // minLat <= posLat = 1
+        minLatCmp.reset(new comparison<field_type>(bp, comparison_n, minLat, posLat, minLatLess, minLatLessOrEq));
+        minLatCmp.get()->generate_r1cs_constraints();
+        bp.add_r1cs_constraint(r1cs_constraint<field_type>(minLatLessOrEq, 1, value_type::one()));
 
-        // 1. userX - areaX = deltaX
-        bp.add_r1cs_constraint(r1cs_constraint<field_type>(userX - areaX, 1, deltaX));
+        // maxLat >= posLat = 1
+        maxLatCmp.reset(new comparison<field_type>(bp, comparison_n, posLat, maxLat, maxLatLess, maxLatLessOrEq));
+        maxLatCmp.get()->generate_r1cs_constraints();
+        bp.add_r1cs_constraint(r1cs_constraint<field_type>(maxLatLessOrEq, 1, value_type::one()));
 
-        // 2. deltaX^2 = deltaX_square
-        bp.add_r1cs_constraint(r1cs_constraint<field_type>(deltaX, deltaX, deltaX_square));
+        // minLng <= posLng = 1
+        minLngCmp.reset(new comparison<field_type>(bp, comparison_n, minLng, posLng, minLngLess, minLngLessOrEq));
+        minLngCmp.get()->generate_r1cs_constraints();
+        bp.add_r1cs_constraint(r1cs_constraint<field_type>(minLngLessOrEq, 1, value_type::one()));
 
-        // 3. userY - areaY = deltaY
-        bp.add_r1cs_constraint(r1cs_constraint<field_type>(userY - areaY, 1, deltaY));
+        // maxLng >= posLng = 1
+        maxLngCmp.reset(new comparison<field_type>(bp, comparison_n, posLng, maxLng, maxLngLess, maxLngLessOrEq));
+        maxLngCmp.get()->generate_r1cs_constraints();
+        bp.add_r1cs_constraint(r1cs_constraint<field_type>(maxLngLessOrEq, 1, value_type::one()));
 
-        // 4. deltaY^2 = deltaY_square
-        bp.add_r1cs_constraint(r1cs_constraint<field_type>(deltaY, deltaY, deltaY_square));
-
-        // 5. deltaX_square + deltaY_square = deltaSum
-        bp.add_r1cs_constraint(r1cs_constraint<field_type>(deltaX_square + deltaY_square, 1, deltaSum));
-
-        // 6. radius^2 = radius_square
-        bp.add_r1cs_constraint(r1cs_constraint<field_type>(radius, radius, radius_square));
-
-        // 7. deltaSum < radius_square
-        cmp.reset(new comparison<field_type>(bp, n, deltaSum, radius_square, less, lessOrEq));
-        cmp.get()->generate_r1cs_constraints();
-        bp.add_r1cs_constraint(r1cs_constraint<field_type>(lessOrEq, 1, out));
+        bp.add_r1cs_constraint(r1cs_constraint<field_type>(minLatLessOrEq + maxLatLessOrEq + minLngLessOrEq + maxLngLessOrEq, 1, out));
     }
 
-    void generate_r1cs_witness(blueprint<field_type> &bp, float ax, float ay, float r, float x, float y) {
-        // to keep all coordinates positive shift them to (1000, 1000)
-        uint coord_shift = 1000;
-        uint precision = 6;
+    void generate_r1cs_witness(blueprint<field_type> &bp, float minLat_, float maxLat_, float minLng_, float maxLng_, float posLat_, float posLng_) {
+        bp.val(minLat) = convert_input(minLat_);
+        bp.val(maxLat) = convert_input(maxLat_);
+        bp.val(minLng) = convert_input(minLng_);
+        bp.val(maxLng) = convert_input(maxLng_);
+        bp.val(posLat) = convert_input(posLat_);
+        bp.val(posLng) = convert_input(posLng_);
 
-        bp.val(areaX) = convert_input(ax + coord_shift, precision);
-        bp.val(areaY) = convert_input(ay + coord_shift, precision);;
-        bp.val(userX) = convert_input(x + coord_shift, precision);;
-        bp.val(userY) = convert_input(y + coord_shift, precision);
-        bp.val(radius) = convert_input(r, precision);;
+        bp.val(out) = 4;
 
-        bp.val(out) = value_type::one();
+        minLatCmp.get()->generate_r1cs_witness();
+        maxLatCmp.get()->generate_r1cs_witness();
+        minLngCmp.get()->generate_r1cs_witness();
+        maxLngCmp.get()->generate_r1cs_witness();
 
-        bp.val(deltaX) = bp.val(userX) - bp.val(areaX);
-        bp.val(deltaY) = bp.val(userY) - bp.val(areaY);
-        bp.val(deltaX_square) = bp.val(deltaX) * bp.val(deltaX);
-        bp.val(deltaY_square) = bp.val(deltaY) * bp.val(deltaY);
-        bp.val(deltaSum) = bp.val(deltaX_square) + bp.val(deltaY_square);
-        bp.val(radius_square) = bp.val(radius) * bp.val(radius);
+        // cout << "Lattiture range: [" << bp.val(minLat).data << ", " << bp.val(maxLat).data <<  "]" << endl;
+        // cout << "Longitude range: [" << bp.val(minLng).data << ", " << bp.val(maxLng).data <<  "]" << endl;
+        // cout << "Position coords: (" << bp.val(posLat).data << ", " << bp.val(posLng).data <<  ")" << endl;
 
-        cmp.get()->generate_r1cs_witness();
-
-        cout << "Area: (" << bp.val(areaX).data << ", " << bp.val(areaY).data <<  ") radius=" << bp.val(radius).data << endl;
-        cout << "User coords: (" << bp.val(userX).data << ", " << bp.val(userY).data <<  ")" << endl;
-        cout << "deltaX = " << bp.val(deltaX).data << endl;
-        cout << "deltaY = " << bp.val(deltaY).data << endl;
-        cout << "deltaX_square = " << bp.val(deltaX_square).data << endl;
-        cout << "deltaY_square = " << bp.val(deltaY_square).data << endl;
-        cout << "deltaSum = " << bp.val(deltaSum).data << endl;
-        cout << "radius_square = " << bp.val(radius_square).data << endl;
-        cout << "less = " << bp.val(less).data << endl;
-        cout << "lessOrEq = " << bp.val(lessOrEq).data << endl;
-        cout << "out = " << bp.val(out).data << endl;
+        // cout << "minLatLess = " << bp.val(minLatLess).data << endl;
+        // cout << "maxLatLess = " << bp.val(maxLatLess).data << endl;
+        // cout << "minLngLess = " << bp.val(minLngLess).data << endl;
+        // cout << "maxLngLess = " << bp.val(maxLngLess).data << endl;
+        // cout << "minLatLessOrEq = " << bp.val(minLatLessOrEq).data << endl;
+        // cout << "maxLatLessOrEq = " << bp.val(maxLatLessOrEq).data << endl;
+        // cout << "minLngLessOrEq = " << bp.val(minLngLessOrEq).data << endl;
+        // cout << "maxLngLessOrEq = " << bp.val(maxLngLessOrEq).data << endl;
     }
 };
--- zkp/bin/main.cpp
+++ zkp/bin/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <boost/algorithm/hex.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
@@ -21,9 +22,11 @@ using namespace std;
 using namespace nil::crypto3::zk::components;
 using namespace nil::crypto3::zk::snark;
 
-int MAX_RADIUS = 1000;
-int MAX_LATITUDE = 90;
-int MAX_LONGITUDE = 180;
+// int MIN_RADIUS = 100;
+// int MAX_RADIUS = 1000;
+// int MAX_LATITUDE = 90;
+// int MAX_LONGITUDE = 180;
+
 
 void pack_verifier_data(
     boost::filesystem::path pout,
@@ -49,92 +52,109 @@ void pack_verifier_data(
         poutf << v;
     }
     poutf.close();
+
+    // convert proof to hex string and print it to output
+    std::string proof_hex;
+    proof_hex.reserve(proof_byteblob.size() * 2);
+    boost::algorithm::hex(proof_byteblob.begin(), proof_byteblob.end(), std::back_inserter(proof_hex));
+    cout << proof_hex << endl;
 }
 
-blueprint<field_type> get_blueprint(float ax, float ay, float r, float x, float y) {
+blueprint<field_type> get_blueprint(float minLat, float maxLat, float minLng, float maxLng, float posLat, float posLng) {
     blueprint<field_type> bp;
     LocationCircuit circuit(bp);
     circuit.generate_r1cs_constraints(bp);
-    circuit.generate_r1cs_witness(bp, ax, ay, r, x, y);
+    circuit.generate_r1cs_witness(bp, minLat, maxLat, minLng, maxLng, posLat, posLng);
     return bp;
 }
 
-bool validate_inputs(float ax, float ay, float r, float x, float y) {
-    // The latitude must be a number between -90 and 90
-    // The longitude must be a number between -180 and 180
-    if (abs(ax) > MAX_LATITUDE) {
-        cout << "Invalid area center latitude. It should be in range [" << -MAX_LATITUDE << ", " << MAX_LATITUDE << "]" << endl;
-        return false;
-    }
-    if (abs(x) > MAX_LATITUDE) {
-        cout << "Invalid user latitude. It should be in range [" << -MAX_LATITUDE << ", " << MAX_LATITUDE << "]" << endl;
-        return false;
-    }
-    if (abs(ay) > MAX_LONGITUDE) {
-        cout << "Invalid area center longitude. It should be in range [" << -MAX_LONGITUDE << ", " << MAX_LONGITUDE << "]" << endl;
-        return false;
-    }
-    if (abs(y) > MAX_LONGITUDE) {
-        cout << "Invalid user longitude. It should be in range [" << -MAX_LONGITUDE << ", " << MAX_LONGITUDE << "]" << endl;
-        return false;
-    }
-    if (r > MAX_RADIUS || r <= 0) {
-        cout << "Radius should be in a range 0..." << MAX_RADIUS << endl;
-        return false;
-    }
+// bool validate_inputs(float minLat, float maxLat, float minLng, float maxLng, float posLat, float posLng) {
+//     // The latitude must be a number between -90 and 90
+//     // The longitude must be a number between -180 and 180
+//     if (abs(ax) > MAX_LATITUDE) {
+//         cout << "Invalid area center latitude. It should be in range [" << -MAX_LATITUDE << ", " << MAX_LATITUDE << "]" << endl;
+//         return false;
+//     }
+//     if (abs(x) > MAX_LATITUDE) {
+//         cout << "Invalid user latitude. It should be in range [" << -MAX_LATITUDE << ", " << MAX_LATITUDE << "]" << endl;
+//         return false;
+//     }
+//     if (abs(ay) > MAX_LONGITUDE) {
+//         cout << "Invalid area center longitude. It should be in range [" << -MAX_LONGITUDE << ", " << MAX_LONGITUDE << "]" << endl;
+//         return false;
+//     }
+//     if (abs(y) > MAX_LONGITUDE) {
+//         cout << "Invalid user longitude. It should be in range [" << -MAX_LONGITUDE << ", " << MAX_LONGITUDE << "]" << endl;
+//         return false;
+//     }
+//     if (r > MAX_RADIUS || r < MIN_RADIUS) {
+//         cout << "Radius should be in a range [" << MIN_RADIUS << "..." << MAX_RADIUS << endl;
+//         return false;
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
 int main(int argc, char *argv[]) {
-    float x, y, r, ax, ay;
+    float minLat, maxLat, minLng, maxLng, posLat, posLng;
+    bool silent;
 
     boost::program_options::options_description options("CLI Proof Generator");
     options.add_options()
-    ("x,x", boost::program_options::value<float>(&x)->default_value(0))
-    ("y,y", boost::program_options::value<float>(&y)->default_value(0))
-    ("r,r", boost::program_options::value<float>(&r)->default_value(10))
-    ("a,a", boost::program_options::value<float>(&ax)->default_value(0))
-    ("b,b", boost::program_options::value<float>(&ay)->default_value(0));
+    ("silent,s", boost::program_options::bool_switch(&silent), "print only hex proof to output")
+    ("minLat,minLat", boost::program_options::value<float>(&minLat)->default_value(0))
+    ("maxLat,maxLat", boost::program_options::value<float>(&maxLat)->default_value(0))
+    ("minLng,minLng", boost::program_options::value<float>(&minLng)->default_value(0))
+    ("maxLng,maxLng", boost::program_options::value<float>(&maxLng)->default_value(0))
+    ("posLat,posLat", boost::program_options::value<float>(&posLat)->default_value(0));
+    ("posLng,posLng", boost::program_options::value<float>(&posLng)->default_value(0));
 
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(options).run(), vm);
     boost::program_options::notify(vm);
 
     cout << setprecision(16);
-    cout << "Area: (" << ax << ", " << ay <<  ") radius=" << r << endl;
-    cout << "User coords: (" << x << ", " << y <<  ")" << endl;
-
-    cout << "Validating input..." << endl;
-    if (!validate_inputs(ax, ay, r, x, y)) {
-        return -1;
+    if (!silent) {
+        cout << "Lattiture range: [" << minLat << ", " << maxLat <<  "]" << endl;
+        cout << "Longitude range: [" << minLng << ", " << maxLng <<  "]" << endl;
+        cout << "Position coords: (" << posLat << ", " << posLng <<  ")" << endl;
     }
 
-    cout << "Getting blueprint..." << endl;
-    blueprint<field_type> bp = get_blueprint(ax, ay, r, x, y);
+    // cout << "Validating input..." << endl;
+    // if (!validate_inputs(ax, ay, r, x, y)) {
+    //     return -1;
+    // }
 
-    cout << "Blueprint is satisfied: " << bp.is_satisfied() << endl;
+    if (!silent) {cout << "Getting blueprint..." << endl;}
+    blueprint<field_type> bp = get_blueprint(minLat, maxLat, minLng, maxLng, posLat, posLng);
+
+    if (!silent) {cout << "Blueprint size: " << bp.num_variables() << endl;}
+
+    if (!silent) {cout << "Blueprint is satisfied: " << bp.is_satisfied() << endl;}
     if (!bp.is_satisfied()) {
         return -1;
     }
 
-    cout << "Generating constraint system..." << endl;
+    if (!silent) {cout << "Generating constraint system..." << endl;}
     const r1cs_constraint_system<field_type> constraint_system = bp.get_constraint_system();
+    if (!silent) {cout << "Number of R1CS constraints: " << constraint_system.num_constraints() << endl;}
 
-    cout << "Generating keypair..." << endl;
+    if (!silent) {cout << "Generating keypair..." << endl;}
     scheme_type::keypair_type keypair = generate<scheme_type>(constraint_system);
 
-    cout << "Generating proof..." << endl;
+    if (!silent) {cout << "Generating proof..." << endl;}
     const scheme_type::proof_type proof = prove<scheme_type>(keypair.first, bp.primary_input(), bp.auxiliary_input());
 
     using basic_proof_system = r1cs_gg_ppzksnark<curve_type>;
     const bool verified = verify<basic_proof_system>(keypair.second, bp.primary_input(), proof);
-    cout << "Verification status: " << verified << endl;
+    if (!silent) {cout << "Verification status: " << verified << endl;}
 
-    if (verified) {
-        cout << "Packing verifier data..." << endl;
-        pack_verifier_data("proof", keypair, proof, bp.primary_input());
+    if (!verified) {
+        return -1;
     }
+
+    if (!silent) {cout << "Packing verifier data..." << endl;}
+    pack_verifier_data("proof", keypair, proof, bp.primary_input());
 
     return 0;
 }
