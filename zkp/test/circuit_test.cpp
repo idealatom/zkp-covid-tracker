9a526df4c4309a00e285ce135ecba4d2b0c129e1
idealatom <idealatom@gmail.com>
2021-07-14 20:24:47 +0700

    add dummy test

---
+++ zkp/test/circuit_test.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/unit_test.hpp>
+
+#define BOOST_TEST_MODULE circuit_test
+
+using namespace nil::crypto3;
+using namespace nil::crypto3::zk;
+
+BOOST_AUTO_TEST_SUITE(circuit_test_suite)
+
+BOOST_AUTO_TEST_CASE(circuit_test) {
+    std::cout << "Test started" << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
